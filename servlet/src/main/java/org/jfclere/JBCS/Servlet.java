6ad094fae94878d5dbeebb7dd9b9984758940aeb
Jean-Frederic Clere <jfclere@gmail.com>
2018-12-16 11:26:28 +0100

    Allow to use different Expires: headers curl -v "http://localhost:8080/demo-1.0-SNAPSHOT/?date=%2B1" (today + 1 day). curl -v "http://localhost:8080/demo-1.0-SNAPSHOT/?date=-1" (today - 1 day). curl -v "http://localhost:8080/demo-1.0-SNAPSHOT/?date=Inv" (invalid)

--- servlet/src/main/java/org/jfclere/JBCS/Servlet.java
+++ servlet/src/main/java/org/jfclere/JBCS/Servlet.java
@@ -3,8 +3,11 @@ package org.jfclere.JBCS;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Random;
+import java.util.TimeZone;
+import java.text.SimpleDateFormat;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -26,7 +29,27 @@ public class Servlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String title = "mod_cache test servlet";
 		response.setHeader("ETag","W/\"5581-1509871979000\"");
-		response.setHeader("Expires", "Wed, 21 Oct 2018 07:28:00 GMT");
+		/* set an Expires according to the parameters */
+                String sdate = request.getParameter("date");
+		if (sdate != null) {
+                        Calendar cal = Calendar.getInstance();
+                        String expires = "Invalid";
+                        if (sdate.equals("+1")) {
+				cal.add(Calendar.DATE, 1);
+				SimpleDateFormat sdf = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss zzz");
+                                sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+                                expires = sdf.format(cal.getTime());
+			} else if (sdate.equals("-1")) {
+				cal.add(Calendar.DATE, -1); 
+				SimpleDateFormat sdf = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss zzz");
+                                sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+                                expires = sdf.format(cal.getTime());
+			}
+			response.setHeader("TraceDateParm", sdate);
+
+			response.setHeader("Expires", expires);
+		} else
+			response.setHeader("Expires", "Wed, 21 Oct 2018 07:28:00 GMT");
 		Date date = new Date();
 		response.setHeader("TraceDate", date.toString());
 		Random rand = new Random();
